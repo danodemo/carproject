7b416890a9dd4889bd7c2e601fcbafec6ddddcd9
Daniel Nicodemo <dan.nicodemo@gmail.com>
2016-04-05 10:16:55 -0400

    changed some things to rebuild

--- SimpleCar.java
+++ SimpleCar.java
@@ -1,5 +1,7 @@
 package simple.carproject;
 
+//import java.util.scanner;
+/*
 public class SimpleCar {
   //These are INITIALIZED, INSTANCE variables, available to objects of the
   //public class, in this case SimpleCar.  So when new.SimpleCar is called,
@@ -10,14 +12,7 @@ public class SimpleCar {
   public boolean isCarStarted= false;
   public int speed = 0;
   public int gasLevel = 10;
-
-  /*CONSTRUCTORS have the SAME NAME AS THE PUBLIC CLASS
-  public SimpleCar() {
-    color = "s";
-    make = "Nissan";
-    moadel = "Altima S";
-  }
-  */
+  
   public SimpleCar(String inputColor, String inputMake, String inputModel) {
     color = inputColor;
 
@@ -30,5 +25,54 @@ public class SimpleCar {
     model = inputModel;
   }
 
+
+}
+*/
+
+public class SimpleCar { 
+
+  public String color; 
+  public String make; 
+  public String model; 
+  public boolean isCarStarted = false; 
+  public int speed = 0; 
+  public int gasLevel = 10; 
+
+  public void startCar() {
+    System.out.println("Starting your car!"); 
+    if (make.equals("BMW")) {
+      System.out.println("Your BMW is broken (again!) - go pay your dealership $4,000 for a starter"); 
+    } else { 
+      isCarStarted = true;  
+    }
+  }
+
+  public void accelerate() { 
+    if (isCarStarted && gasLevel > 0) { 
+      speed = speed + 5; 
+    } else { 
+      System.out.println("You have to start your car before you accelerate"); 
+      if (make.equals("BMW")) { 
+        System.out.println("Sorry, BMW Owner, you don't get to accelerate"); 
+      }
+    }
+  }
+
+  public SimpleCar() { 
+    color = "Blue";
+    make = "Land Rover"; 
+    model = "Range Rover Sport SVR"; 
+  }
+
+  public SimpleCar(String inputColor, String inputMake, String inputModel) {
+    color = inputColor; 
+    if (inputMake.equals("Dodge")) { 
+      make = "CrappyCar"; 
+      speed = 50; 
+    } else { 
+      make = inputMake; 
+    }
+    model = inputModel;  
+  }
 
 }
---
+++ SimpleDriver.java
@@ -0,0 +1,10 @@
+package simple.carproject; 
+
+public class SimpleDriver { 
+  public static void main(String[] args) { 
+    System.out.println("Welcome to the Garage."); 
+
+    //SimpleCar mySimpleCar = new SimpleCar(); 
+    //System.out.println("mySimpleCar is a " + mySimpleCar.make); 
+  }
+}
