20cd3a3252b65cd0914e15a134fb0f606721f60e
Daniel Nicodemo <dan.nicodemo@gmail.com>
2016-04-06 13:23:04 -0400

    OMG SO MUCH FUNCTIONALITY

--- SimpleCar.java
+++ SimpleCar.java
@@ -2,34 +2,6 @@ package simple.carproject;
 
 import java.util.Scanner;
 
-/*
-public class SimpleCar {
-  //These are INITIALIZED, INSTANCE variables, available to objects of the
-  //public class, in this case SimpleCar.  So when new.SimpleCar is called,
-  //It will have all the following variables ready or assigned to it
-  public String color;
-  public String make;
-  public String model;
-  public boolean isCarStarted= false;
-  public int speed = 0;
-  public int gasLevel = 10;
-  
-  public SimpleCar(String inputColor, String inputMake, String inputModel) {
-    color = inputColor;
-
-      if (inputMake.equals("Dodge")) {
-       make = "Piece of Shit"; }
-      else {
-        make = inputMake;
-      }
-
-    model = inputModel;
-  }
-
-
-}
-*/
-
 public class SimpleCar { 
 
   public String color; 
@@ -40,39 +12,76 @@ public class SimpleCar {
   public int gasLevel = 10; 
 
   public void startCar() {
+    if (!isCarStarted){
     System.out.println("Starting your car!"); 
-    if (make.equals("BMW")) {
-      System.out.println("Your BMW is broken (again!) - go pay your dealership $4,000 for a starter"); 
-    } else { 
-      isCarStarted = true;  
+    isCarStarted = true; }
+    else {
+      System.out.println("Your car is already running, dumbass.  Now your starter is fucked.");
     }
   }
 
-  public void accelerate() { 
-    if (isCarStarted && gasLevel > 0) { 
-      speed = speed + 5; 
-    } else { 
-      System.out.println("You have to start your car before you accelerate"); 
-      if (make.equals("BMW")) { 
-        System.out.println("Sorry, BMW Owner, you don't get to accelerate"); 
+  public void stopCar() {
+    if (isCarStarted) {
+      isCarStarted = false;
+    } else {
+      System.out.println("Your car isn't running, idiot.");
+    }
+  }
+
+  public void accelerate()throws Exception { 
+    if (isCarStarted && gasLevel > 0) {
+      System.out.println("Found the long, skinny pedal. Pressing it!");
+        speed = speed + 5;
+        gasLevel = gasLevel - 1;
       }
+      else {
+        if (gasLevel < 1) { 
+          throw new Exception("Dude you're out of gas!!!  Better fill up!");
+
+        } else if (!isCarStarted) { 
+          throw new Exception("Maybe try starting the car first, brah..."); 
+        }
+      }
+  }
+
+  public void brake() {
+    if (speed > 0) {
+      System.out.println("Slowing down...your speed is now " + speed + "MPH.");
+      speed = (speed - 5);
+    } else {
+      System.out.println("You aren't moving, so there's no need to break.");
+    }
+  }
+
+  public void fillUp() {
+    System.out.println("Filling up the tank!");
+    gasLevel = 20;
+    try {
+      Thread.sleep(3000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+    }
+    System.out.println("Your tank is full!");
+  }
+
+  public void printInfo() {
+    System.out.println("You are driving a " + color + " " + make + " " + model + ".");
+      if (isCarStarted){
+      System.out.println("You are moving at " + speed + " mph, and currently have " + gasLevel + " gallons of fuel remaining.");
+    } else {
+      System.out.println("Your car is currently not running.  You have " + gasLevel + " gallons of fuel remaining.");
     }
   }
 
   public SimpleCar() { 
-    color = "Blue";
-    make = "Land Rover"; 
-    model = "Range Rover Sport SVR"; 
+    color = "Red";
+    make = "Shelby"; 
+    model = "Mustang"; 
   }
 
   public SimpleCar(String inputColor, String inputMake, String inputModel) {
-    color = inputColor; 
-    if (inputMake.equals("Dodge")) { 
-      make = "CrappyCar"; 
-      speed = 50; 
-    } else { 
-      make = inputMake; 
-    }
+    color = inputColor;
+    make = inputMake;
     model = inputModel;  
   }
 
--- SimpleDriver.java
+++ SimpleDriver.java
@@ -3,13 +3,21 @@ package simple.carproject;
 import java.util.Scanner;
 
 public class SimpleDriver { 
-  public static void main(String[] args) { 
-    Scanner lineScanner = new Scanner(System.in);
 
-    System.out.println("Welcome to the Garage!");
-    System.out.println("Let's build your dream car and take it for a spin!");
-    //wait
-    
+  public static void carCommands(){
+    System.out.println("What would you like to do now? Please pick a number!");
+      System.out.println("1. Start Car");
+      System.out.println("2. Accelerate");
+      System.out.println("3. Brake");
+      System.out.println("4. Stop Car");
+      System.out.println("5. Print Car Info");
+      System.out.println("6. Exit");
+      System.out.println("7. Activate the Hellgate");
+  }
+  
+  public static void playGame(){
+    //GET CAR INFO STARTS HERE
+    Scanner lineScanner = new Scanner(System.in);
     System.out.println("What color is your car?");
       String carColor = lineScanner.nextLine();
     System.out.println("Alright, your car is " + carColor + "!");
@@ -22,15 +30,97 @@ public class SimpleDriver {
       String carModel = lineScanner.nextLine();
 
     SimpleCar yourCar = new SimpleCar(carColor, carMake, carModel); //add parameters to trigger
-    
+    System.out.println("");
     System.out.println("Alright, your car is a " + carColor + " " + carMake + " " + carModel + "!");
+    //CAR INFO ENDS HERE
 
+    //DESTINATION INFO STARTS HERE
+    String destination = " "; //destination
+    int distance = 0; //distance to travel = 1 to 1 with gasLevel
+    System.out.println("You are ready to drive!  Where would you like to go?");
+    System.out.println("1. The Bank - 2. The Grocery Store - 3. The Club");
+    String destChoice = lineScanner.nextLine();
+    if (destChoice.equals("1")){
+      destination = "The Bank";
+      distance = 5;
+    }
+    else if (destChoice.equals("2")){
+      destination = "The Grocery Store";
+      distance = 3;
+    }
+    else if (destChoice.equals("3")){
+      destination = "The Club";
+      distance = 10;
+    }
+    else {
+      System.out.println("That's not a valid destination, and now your car has exploded.");
+    }
+    System.out.println("You have chosen " + destination + " and it is " + distance + " miles away!");
+    System.out.println("You have " + yourCar.gasLevel + " units of fuel.");
+    System.out.println("Now to drive!");
+    //DESTINATION INFO ENDS HERE
+
+    try {
+      Thread.sleep(2000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+    }
+    System.out.print("\033[H\033[2J");
+
+    //USER ACTION MENU
+    for (int counter = 0; counter < 1000; counter ++) { //ACTION CHOICE LOOP BEGINS
+      System.out.println(" ");
+      carCommands();
+      String userAction = lineScanner.nextLine();
+      System.out.print("\033[H\033[2J");
+
+      boolean userChoseOptionOne = userAction.equals("1");
+      boolean userChoseOptionTwo = userAction.equals("2");
+      boolean userChoseOptionThree = userAction.equals("3");
+      boolean userChoseOptionFour = userAction.equals("4");
+      boolean userChoseOptionFive = userAction.equals("5");
+      boolean userChoseOptionSix = userAction.equals("6");
+      boolean userChoseOptionSeven = userAction.equals("7");
+      if (userChoseOptionOne) {
+        yourCar.startCar();
+      }
+      else if (userChoseOptionTwo) {
+          try {
+          yourCar.accelerate();
+        } catch (Exception myCustomException){
+          System.out.println(myCustomException.getMessage()); 
+          //myCustomException.printStackTrace(); 
+        }
+      }
+      else if (userChoseOptionThree) {
+        yourCar.brake();
+      }
+      else if (userChoseOptionFour) {
+        yourCar.stopCar();
+      }
+      else if (userChoseOptionFive) {
+        yourCar.printInfo();
+      }
+      else if (userChoseOptionSix) {
+        System.out.println("Thanks for visiting the garage!  Be safe out there!");
+        break;
+      }
+      else if (userChoseOptionSeven) {
+        System.out.println("We're sorry, the hellgate is currently closed.");
+      }
+      else {
+        System.out.println("That's not a valid CHOICE, you asshole!");
+      }
+    }
+  
+  }
+
+
+
+  public static void main(String[] args) { 
+    System.out.println("Welcome to the Garage!");
+    System.out.println("Let's build your dream car and take it for a spin!");
+    //wait or Clear Screen
+    playGame();
   }
 }
-
-
-/*
-String carColor = "Red";
-String carMake = "Ford";
-String carModel = "Mustang";
-*/
